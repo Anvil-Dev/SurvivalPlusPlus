1c2fbb1eb8bbcebfec3de90ef78bf273021c92ed
Gugle <administration@ztxy666.cn>
2025-07-26 22:34:59 +0800

    使得大木桶可以正常输出比较器信号

--- src/main/java/dev/anvilcraft/rg/survival/mixin/BarrelBlockMixin.java
+++ src/main/java/dev/anvilcraft/rg/survival/mixin/BarrelBlockMixin.java
@@ -5,15 +5,21 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.anvilcraft.rg.survival.SurvivalPlusPlusServerRules;
 import dev.anvilcraft.rg.survival.util.LargeBarrelUtil;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.Container;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BarrelBlock;
 import net.minecraft.world.level.block.entity.BarrelBlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.OptionalInt;
 
@@ -41,5 +47,21 @@ abstract class BarrelBlockMixin {
             .apply(LargeBarrelUtil.MENU_PROVIDER_COMBINER)
             .orElse(null);
         return original.call(instance, provider);
+    }
+
+    @Inject(
+        method = "getAnalogOutputSignal",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void getAnalogOutputSignal(BlockState blockState, @NotNull Level level, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
+        if (!SurvivalPlusPlusServerRules.largeBarrel) return;
+        BlockEntity entity = level.getBlockEntity(pos);
+        if (!(entity instanceof BarrelBlockEntity barrelBlockEntity)) return;
+        if (!LargeBarrelUtil.isLargeBarrel(level, pos)) return;
+        Container container = LargeBarrelUtil.combine(barrelBlockEntity, level, pos)
+            .apply(LargeBarrelUtil.CONTAINER_COMBINER)
+            .orElse(null);
+        cir.setReturnValue(AbstractContainerMenu.getRedstoneSignalFromContainer(container));
     }
 }
